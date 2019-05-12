db9cbb744063946026dbc7c99c6333afb4388e2b
Andy Brace <github@openbook.uk.com>
2019-05-12 16:36:20 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7bf7f3ac7ef163aecee2.js"
+    "url": "webpack-runtime-488aa897434ef2734103.js"
   },
   {
     "url": "styles.551405ccd3a11579df5a.css"
@@ -35,17 +35,17 @@ self.__precacheManifest = [
     "url": "styles-dd15041184045bdd7a3d.js"
   },
   {
-    "url": "app-5f657fa5ba8f5e045adf.js"
+    "url": "app-5e6ce7ed48f16c29a869.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-9cf1de5eca3c4720b943.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "912481ed5bb40fc8e7f336b68d847b53"
+    "revision": "bb687a428d66aa76fb4a8a92a71b55f7"
   },
   {
-    "url": "component---packages-gatsby-theme-andybrace-src-pages-404-mdx-01492a2daf7b77b0827f.js"
+    "url": "component---packages-gatsby-theme-andybrace-src-pages-404-mdx-0369d943d9246dba85f7.js"
   },
   {
     "url": "0-4d0f3e60991ffa26ecdc.js"
